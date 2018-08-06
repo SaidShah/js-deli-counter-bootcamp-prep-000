67910330b6a14d2a38191266b19149cfd7b3c89d
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-06 19:20:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 
   var katzDeli=[]
 
+
+
+function takeANumber(line){
+  var counter =0;
+  counter++
+  line.push(counter)
+  return `Welcome! You're ticket number #${counter}`
+}
+
+takeANumber(line) // "Welcome! You're ticket number #1" [1]
+takeANumber(line) // "Welcome! You're ticket number #2" [1, 2]
+
+nowServing(line) // "Currently serving #1." [2]
+nowServing(line) // "Currenlty serving #2." []
+
+takeANumber(line) // "Welcome! You're ticket number #3" [3]
+
+
   
 function takeANumber(line, name){
 
